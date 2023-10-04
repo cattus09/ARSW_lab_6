7288646065d76f08855f672572f879b192999fba
NicolasCastro9 <tankiandown@gmail.com>
2023-10-04 09:30:47 -0500

    backend final

--- target/classes/static/js/app.js
+++ target/classes/static/js/app.js
@@ -3,11 +3,13 @@ var apimock = apimock;
 var app = (function (){
     var author;
     var blueprintName;
+    var totalPuntos;
 
+    // actualiza el contenido de author del HTML  mostrando un mensaje
     function getName() {
             $("#name").text(author + "'s " + "blueprints:");
     }
-
+    //  obtener blueprints del autor especificado, si no se manda nada aparece el mensaje
     function getNameAuthorBlueprints() {
         author = $("#author").val();
         if (author === "") {
@@ -16,60 +18,65 @@ var app = (function (){
             apimock.getBlueprintsByAuthor(author,authorData);
         }
     }
+    // utiliza el método map() para tomar una lista de blueprints y transformar cada elemento 
+    // de la lista en un nuevo objeto que contiene solo el nombre y la cantidad de puntos asociados a ese bluePrint.
+    var authorData = function( data) {
+        $("#table tbody").empty();
+            if (data === undefined) {
+                alert("No existe el autor");
+                $("#name").empty();
+                $("#points").text("Total Points");
+                $("#nameblu").empty();
+            } else {
+                getName();
+                const datanew = data.map((elemento) => {
+                    return {
+                        name: elemento.name,
+                        puntos: elemento.points.length
+                    }
+                });
+            //  toma cada uno de los elementos del arreglo datanew 
+            //  y generar filas de una tabla en un documento HTML utilizando jQuery
+            datanew.map((elementos) => {
+                $("#table > tbody:last").append($("<tr><td>" + elementos.name + "</td><td>" + elementos.puntos.toString() +
+                "</td><td>" + "<button  id=" + elementos.name + " onclick=app.getBlueprintByAuthorAndName(this)>open</button>"
+                 + "</td>"));
+            });
+            // se realiza una operación de reducción (reduce) sobre el arreglo datanew para calcular la suma 
+            //total de puntos de todos los planos del usuario,
+            //y luego se actualiza el contenido de un elemento en el documento HTML
+            totalPuntos = datanew.reduce((suma, {puntos}) => suma + puntos, 0);
 
-     var authorData = function( data) {
-         $("#table tbody").empty();
-         if (data === undefined) {
-             alert("No existe el autor");
-             $("#name").empty();
-             $("#points").text("Total Points");
-             $("#nameblu").empty();
-         } else {
-             getName();
-             const datanew = data.map((elemento) => {
-                 return {
-                     name: elemento.name,
-                     puntos: elemento.points.length
-                 }
-             });
+            $("#points").text("Total user points: " + totalPuntos);
+        }
+    }
 
-             datanew.map((elementos) => {
-                 $("#table > tbody:last").append($("<tr><td>" + elementos.name + "</td><td>" + elementos.puntos.toString() +
-                     "</td><td>" + "<button  id=" + elementos.name + " onclick=app.getBlueprintByAuthorAndName(this)>open</button>" + "</td>"));
-             });
+    function getBlueprintByAuthorAndName(data) {
+        author = $("#author").val();
+        blueprintName = data.id;
+        $("#nameblu").text("Current blueprint: " + blueprintName);
+        apimock.getBlueprintByAuthorAndName(author, blueprintName, printPoints);
+    }
 
-             const totalPuntos = datanew.reduce((suma, {puntos}) => suma + puntos, 0);
-
-             $("#points").text("Total user points: " + totalPuntos);
+    function printPoints(data) {
+        const puntos = data.points;
+        var c = document.getElementById("myCanvas");
+        var ctx = c.getContext("2d");
+        ctx.clearRect(0, 0, c.width, c.height);
+        ctx.restore();
+        ctx.beginPath();
+        for (let i = 1; i < puntos.length; i++) {
+            ctx.moveTo(puntos[i - 1].x, puntos[i - 1].y);
+            ctx.lineTo(puntos[i].x, puntos[i].y);
+            if (i === puntos.length - 1) {
+                ctx.moveTo(puntos[i].x, puntos[i].y);
+                ctx.lineTo(puntos[0].x, puntos[0].y);
             }
-         }
-
-         function getBlueprintByAuthorAndName(data) {
-                 author = $("#author").val();
-                 blueprintName = data.id;
-                 $("#nameblu").text("Current blueprint: " + blueprintName);
-                 apimock.getBlueprintByAuthorAndName(author, blueprintName, printPoints);
-             }
-
-         function printPoints(data) {
-                 const puntos = data.points;
-                 var c = document.getElementById("myCanvas");
-                 var ctx = c.getContext("2d");
-                 ctx.clearRect(0, 0, c.width, c.height);
-                 ctx.restore();
-                 ctx.beginPath();
-                 for (let i = 1; i < puntos.length; i++) {
-                     ctx.moveTo(puntos[i - 1].x, puntos[i - 1].y);
-                     ctx.lineTo(puntos[i].x, puntos[i].y);
-                     if (i === puntos.length - 1) {
-                         ctx.moveTo(puntos[i].x, puntos[i].y);
-                         ctx.lineTo(puntos[0].x, puntos[0].y);
-                     }
-                 }
-                 ctx.stroke();
-             }
-     return{
+        }
+        ctx.stroke();
+    }
+    return{
         getBlueprintByAuthorAndName:getBlueprintByAuthorAndName,
         getNameAuthorBlueprints: getNameAuthorBlueprints
-     }
+    }
 })();
